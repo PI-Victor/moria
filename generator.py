6ef3b9cf2b4730d84e4efb1c1fc0b90d30dc20a7
P-Victor <irap4fun@gmail.com>
2014-08-28 20:50:27 +0200

    initial commit

---
+++ generator.py
@@ -0,0 +1,19 @@
+#!.venv/bin/python
+
+import ConfigParser
+import time
+import random
+
+
+"""read the config, generalise, adjust threshold and generate
+"""
+
+
+
+if __name__ == '__main__':
+    
+
+
+
+
+
