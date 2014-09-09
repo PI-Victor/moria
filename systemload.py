aa2b52080590082af37ca7befb7eb2201c1a49e5
PI-Victor <irap4fun@gmail.com>
2014-09-09 22:52:07 +0200

    fixed unrefrenced vars

--- systemload.py
+++ systemload.py
@@ -4,48 +4,56 @@ import datetime
 import pygal
 import random
 from pygal.style import NeonStyle
+import daemon
+import logging
+import os
 
 avg = 1
 sample = 10 
+
+debug_log = os.path.join(os.path.sep, os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'debug.log')
+
+logging.basicConfig(filename=debug_log, format='%(asctime)s %(levelname)s:%(messages)s',
+                    filemode='a', level=logging.DEBUG)
 
 def create_timeseries(kpi_tuple):
     time_series = {}
     for point in range(sample):
         datapoints = []
         [time_series.setdefault(metric,[]) for metric in kpi_tuple._fields]
-        [values.append(getattr(cpu_load, metric)) for metric,values in time_series.items()]
+        [values.append(getattr(kpi_tuple, metric)) for metric,values in time_series.items()]
     return time_series
 
-def get_cpu_metrics():
-    cpuload_tuple = psutil.cpu_times_percent(interval=cpuint, percpu=percore)
+def cpu_metrics():
+    cpuload_tuple = psutil.cpu_times_percent(interval=avg, percpu=False)
     time_series = create_timeseries(cpuload_tuple)
     print time_series
 
-def get_vmem_metrics():
+def vmem_metrics():
     vmem_tuple = psutil.virtual_memory()
     time_series = create_timeseries(vmem_tuple)
     print time_series
 
-def swap_memory():
+def swapmem_metrics():
     swapmem_tuple = psutil.swap_memory()
     time_series = create_timeseries(swapmem_tuple)
     print time_series
 
-def create_graph(kpi_list):
+def create_graph(kpi_list, chart_name='chart.svg'):
     labels = []
     for label in kpi_list:
         labels.append(label)
-    chart = pygal.StackedLine(fill=True,width=1024, height=600, style=NeonStyle)
+    chart = pygal.Line(fill=True,width=1024, height=600, style=NeonStyle)
     chart.x_labels = map(str, range(0,10))
     for line, series in kpi_list.items():
         print line, series
         chart.add(line,series)
-    chart.render_to_file('chart.svg')
-
+    chart.render_to_file(chart_name)
 
 def main():
-    while True:
-
+    cpu_metrics()
+    vmem_metrics()
+    swapmem_metrics()
 
 if __name__ == '__main__':
     main()
