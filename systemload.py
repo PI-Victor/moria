40135b5ce7d159af75ab81b08c487673e4bf7ca9
PI-Victor <irap4fun@gmail.com>
2014-09-09 22:36:31 +0200

    functions for vmem, swapmem added

--- systemload.py
+++ systemload.py
@@ -8,36 +8,44 @@ from pygal.style import NeonStyle
 avg = 1
 sample = 10 
 
-def create_timeseries(cpuint=1,percore=False):
-#    O(2^n)
+def create_timeseries(kpi_tuple):
     time_series = {}
-
-    for i in range(sample):
+    for point in range(sample):
         datapoints = []
-        cpu_load = psutil.cpu_times_percent(interval=cpuint, percpu=percore)
-        [time_series.setdefault(metric,[]) for metric in cpu_load._fields]
+        [time_series.setdefault(metric,[]) for metric in kpi_tuple._fields]
         [values.append(getattr(cpu_load, metric)) for metric,values in time_series.items()]
-#        [v.append(random.randrange(10)) for k,v in time_series.items()]   # for random quick testing of a graph
     return time_series
+
+def get_cpu_metrics():
+    cpuload_tuple = psutil.cpu_times_percent(interval=cpuint, percpu=percore)
+    time_series = create_timeseries(cpuload_tuple)
+    print time_series
+
+def get_vmem_metrics():
+    vmem_tuple = psutil.virtual_memory()
+    time_series = create_timeseries(vmem_tuple)
+    print time_series
+
+def swap_memory():
+    swapmem_tuple = psutil.swap_memory()
+    time_series = create_timeseries(swapmem_tuple)
+    print time_series
 
 def create_graph(kpi_list):
     labels = []
     for label in kpi_list:
         labels.append(label)
-
     chart = pygal.StackedLine(fill=True,width=1024, height=600, style=NeonStyle)
     chart.x_labels = map(str, range(0,10))
-
     for line, series in kpi_list.items():
         print line, series
         chart.add(line,series)
-
     chart.render_to_file('chart.svg')
+
 
 def main():
     while True:
-        kpis = create_timeseries(avg)
-        create_graph(kpis)
+
 
 if __name__ == '__main__':
     main()
