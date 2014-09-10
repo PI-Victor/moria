dfca60f4537ecc1786c1102b377d3ff6cabb82e3
PI-Victor <irap4fun@gmail.com>
2014-09-10 02:01:19 +0200

    moved functions to end of file

--- systemload.py
+++ systemload.py
@@ -8,39 +8,29 @@ import daemon
 import logging
 import os
 import sys
-#TODO : Must implement multiproc and a daemon for precise computation of system usage
-#execution is sequential at this point, and the sample parameter delays data for the
-#next time series
 
+'''TODO : Must implement multiproc and a daemon for precise computation of system usage
+execution is sequential at this point, and the sample parameter delays data for the
+next time series
+'''
 
 if len(sys.argv) < 2:
     sample = 15 #how many sample to currently grab for each graph
-    print 'You can send the number of samples, as a parameter, needed for the graphs, default is 10'
+    print 'You can send the number of samples, as a parameter, needed for the graphs, default is 15'
 else:
     try:
         sample = int(sys.argv[1])
     except ValueError:
-        print "That's not an integer, reverting to default 10 samples"
+        print "That's not an integer, reverting to default 15 samples"
         sample = 15
 
 avg = 1 #time to sample cpu usage
-
 work_dir = os.path.join(os.path.sep, os.path.dirname(os.path.realpath(__file__)))
 debug_log = os.path.join(os.path.sep, work_dir, 'debug.log')
 graph_dir = os.path.join(os.path.sep, work_dir, 'graphs')
 
 logging.basicConfig(filename=debug_log, format='%(asctime)s %(levelname)s:%(message)s',
                     filemode='a', level=logging.DEBUG)
-
-def create_timeseries(psfunct, *args):
-    time_series = {}
-    for point in range(sample):
-        datapoints = []
-        kpi_tuple = psfunct(*args)
-        [time_series.setdefault(metric,[]) for metric in kpi_tuple._fields]
-        [values.append(getattr(kpi_tuple, metric)) for metric,values in time_series.items()]
-        time.sleep(1.5)
-    return time_series
 
 def cpu_metrics():
     logging.debug('Fetching Cpu Load metrics')
@@ -73,6 +63,16 @@ def diskio_metrics():
     diskusage_funct = psutil.disk_io_counters
     metric_sample = create_timeseries(diskusage_funct)
     create_graph(metric_sample, 'diskio.svg', 'Disk IO counters')
+
+def create_timeseries(psfunct, *args):
+    time_series = {}
+    for point in range(sample):
+        datapoints = []
+        kpi_tuple = psfunct(*args)
+        [time_series.setdefault(metric,[]) for metric in kpi_tuple._fields]
+        [values.append(getattr(kpi_tuple, metric)) for metric,values in time_series.items()]
+        time.sleep(1.5)
+    return time_series
 
 def create_graph(kpi_list, chart_name='chart.svg', chart_title='default'):
     labels = []
