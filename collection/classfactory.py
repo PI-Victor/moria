edb21a3a00a3f2c7c5d6b54c5cc241c2e43363f1
PI-Victor <irap4fun@gmail.com>
2014-09-23 23:38:09 +0200

    refactored functions to class, moved modules to collection

---
+++ collection/classfactory.py
@@ -0,0 +1,98 @@
+import psutil
+import pygal
+import time
+import os
+import datetime
+
+from pygal.style import NeonStyle
+
+work_dir = os.path.join(os.path.sep, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+debug_log = os.path.join(os.path.sep, work_dir, 'debug.log')
+graph_dir = os.path.join(os.path.sep, work_dir, 'graphs')
+
+class BaseClass(object):
+    def __init__(self, *args):
+        self.graph_fill = True
+        self.graph_width = 1440
+        self.graph_height = 500
+        self.legend = True
+    
+    def create_timeseries(self,*args):
+        time_series = {}
+        for point in range(self.sample):
+            datapoints = []
+            kpi_tuple = self.psfunct(*args)
+            [time_series.setdefault(metric,[]) for metric in kpi_tuple._fields]
+            [values.append(getattr(kpi_tuple, metric)) for metric,values in time_series.items()]
+            time.sleep(self.interval)
+        self.create_graph(time_series)
+
+    def create_graph(self, kpi_list):
+        labels = []
+        for label in kpi_list:
+            labels.append(label)
+            chart = pygal.Line(fill=True, width=self.graph_width, height=self.graph_height, title=self.graph_tag, 
+                               legend_at_bottom=self.legend)
+            chart.x_labels = map(str, range(0,self.sample))
+            for line, series in kpi_list.items():
+                chart.add(line,series)
+            chart.render_to_file(os.path.join(os.path.sep, graph_dir, self.template))
+
+
+class CpuMetrics(BaseClass):
+    def __init__(self, template='cpu_load.svg', graph_tag='Cpu Load Total', sample=5, interval=1):
+        self.message = 'Cpu Processing started'
+        self.template = template
+        self.graph_tag = graph_tag
+        self.psfunct = psutil.cpu_times_percent
+        self.sample = sample
+        self.interval = interval
+        self.percpu = False
+        BaseClass.__init__(self)
+
+    def create_timeseries(self):
+        BaseClass.create_timeseries(self, self.interval, self.percpu)
+
+    
+class VmMetrics(BaseClass):
+    def __init__(self, template='vmem.svg', graph_tag='Virtual Memory', sample=5, interval=1):
+        self.message = 'Fetching Virtual Memory Metrics'
+        self.template = template
+        self.graph_tag = graph_tag
+        self.sample = sample
+        self.interval = interval
+        self.psfunct = psutil.virtual_memory
+        BaseClass.__init__(self)
+
+        
+class SwapMetrics(BaseClass):
+    def __init__(self, template='swap_mem.svg', graph_tag='Swap memory', sample=5, interval=5):
+        self.message = 'Fetching Swap Memory Metrics'
+        self.template = template
+        self.graph_tag = graph_tag
+        self.sample = sample
+        self.interval = interval
+        self.psfunct = psutil.swap_memory
+        BaseClass.__init__(self)
+
+
+class NetIoMetrics(BaseClass):
+    def __init__(self, template='netio.svg', graph_tag='Network IO', sample=5, interval=5):
+        self.message = 'Fetching NIC Usage Metrics'
+        self.template = template
+        self.graph_tag = graph_tag
+        self.sample = sample
+        self.interval = interval
+        self.psfunct = psutil.net_io_counters
+        BaseClass.__init__(self)
+
+
+class DiskIoMetrics(BaseClass):
+    def __init__(self, template='diskio.svg', graph_tag='Diso IO Metrics', sample=5, interval=5):
+        self.message = 'Fetching Disk IO'
+        self.template = template
+        self.graph_tag = graph_tag
+        self.sample = sample
+        self.interval = interval
+        self.psfunct = psutil.disk_io_counters
+        BaseClass.__init__(self)
--- systemload.py
+++ systemload.py
@@ -3,9 +3,9 @@ import sys
 import random
 import time 
 import datetime
-import pygal
-from pygal.style import NeonStyle
-import psutil
+
+from collection.classfactory import CpuMetrics, VmMetrics, NetIoMetrics, DiskIoMetrics, SwapMetrics
+
 import daemon
 import logging
 
@@ -38,107 +38,15 @@ graph_dir = os.path.join(os.path.sep, work_dir, 'graphs')
 logging.basicConfig(filename=debug_log, format='%(asctime)s %(levelname)s:%(message)s',
                     filemode='a', level=logging.DEBUG)
 
-class BaseClass(object):
-    def __init__(self, template='', tag='', message='', *args):
-        self.tag = tag
-        self.template = template
-        self.message = message
-        self.graph_fill = True
-        self.graph_width = 1440
-        self.graph_height = 500
-        self.legend = True
-    
-    def create_timeseries(self, psfunct, *args):
-        time_series = {}
-        for point in range(sample):
-            datapoints = []
-            kpi_tuple = psfunct(*args)
-            [time_series.setdefault(metric,[]) for metric in kpi_tuple._fields]
-            [values.append(getattr(kpi_tuple, metric)) for metric,values in time_series.items()]
-            time.sleep(1.5)
-        return time_series
-
-    def create_graph(self, kpi_list):
-        labels = []
-        for label in kpi_list:
-            labels.append(label)
-            chart = pygal.Line(fill=True, width=self.width, height=self.width, title=self.template, 
-                               legend_at_bottom=self.legend)
-            chart.x_labels = map(str, range(0,sample))
-            for line, series in kpi_list.items():
-                chart.add(line,series)
-                chart.render_to_file(os.path.join(os.path.sep, graph_dir, chart_name))
-
-        
-class CpuMetrics(BaseClass):
-    def do_graph(self):
-        self.create_timeseries()
-        
-        
-
-
-def cpu_metrics():
-    logging.debug('Fetching Cpu Load metrics')
-    cpuload_funct = psutil.cpu_times_percent 
-    interval=avg 
-    percpu=False
-    metric_sample = create_timeseries(cpuload_funct, interval, percpu)
-    create_graph(metric_sample, 'cpu_load.svg', 'Cpu Load')
-
-def vmem_metrics():
-    logging.debug('Fetching Virtual Memory metrics')
-    vmem_funct = psutil.virtual_memory
-    metric_sample = create_timeseries(vmem_funct)
-    create_graph(metric_sample, 'vmem.svg', 'Virtual Memory')
-
-def swapmem_metrics():
-    logging.debug('Fetching Swap Memory metrics')
-    swapmem_funct = psutil.swap_memory
-    metric_sample = create_timeseries(swapmem_funct)
-    create_graph(metric_sample,'swap_mem.svg','Swap Memory')
-
-def netio_metrics():
-    logging.debug('Fetching NIC Usage metrics')
-    netusage_funct = psutil.net_io_counters
-    metric_sample = create_timeseries(netusage_funct)
-    create_graph(metric_sample, 'netio.svg','Network IO')
-
-def diskio_metrics():
-    logging.debug('Fetching disk io')
-    diskusage_funct = psutil.disk_io_counters
-    metric_sample = create_timeseries(diskusage_funct)
-    create_graph(metric_sample, 'diskio.svg', 'Disk IO counters')
-
-def create_timeseries(psfunct, *args):
-    time_series = {}
-    for point in range(sample):
-        datapoints = []
-        kpi_tuple = psfunct(*args)
-        [time_series.setdefault(metric,[]) for metric in kpi_tuple._fields]
-        [values.append(getattr(kpi_tuple, metric)) for metric,values in time_series.items()]
-        time.sleep(1.5)
-    return time_series
-
-def create_graph(kpi_list, chart_name='chart.svg', chart_title='default'):
-    labels = []
-    for label in kpi_list:
-        labels.append(label)
-    chart = pygal.Line(fill=True, width=1440, height=500, title=chart_title, 
-                      legend_at_bottom=True)
-    chart.x_labels = map(str, range(0,sample))
-    for line, series in kpi_list.items():
-        chart.add(line,series)
-    chart.render_to_file(os.path.join(os.path.sep, graph_dir, chart_name))
-
 def main():
     daemon_context = daemon.DaemonContext(files_preserve = [logging.FileHandler(debug_log)])
     #with daemon_context:
     while True:
-            cpu_metrics()
-            vmem_metrics()
-            swapmem_metrics()
-            netio_metrics()
-            diskio_metrics()
-
+        CpuMetrics().create_timeseries()
+        VmMetrics().create_timeseries()
+        SwapMetrics().create_timeseries()
+        NetIoMetrics().create_timeseries()
+        DiskIoMetrics().create_timeseries()
+            
 if __name__ == '__main__':
     main()
