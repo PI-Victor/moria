71db73a2e0ded17003d3efccc7b728d6d7074af2
PI-Victor <palade.ionut@gmail.com>
2014-12-30 12:55:41 +0100

    moved if clause into main

--- systemload.py
+++ systemload.py
@@ -32,26 +32,14 @@ import daemon
 from collection.classfactory import CpuMetrics, VmMetrics
 from collection.classfactory import NetIoMetrics, DiskIoMetrics, SwapMetrics
 
-'''TODO : Must implement multiproc and a daemon for precise computation of system usage
-execution is sequential at this point, and the sample parameter delays data for the
-next time series
-'''
-
-if len(sys.argv) < 2:
-    sample = 15 #how many sample to currently grab for each graph
-    print 'You can send the number of samples, as a parameter, needed for the graphs, default is 15'
-else:
-    try:
-        sample = int(sys.argv[1])
-    except ValueError:
-        print "That's not an integer, reverting to default 15 samples"
-        sample = 15
-
+#TODO : Must implement multiproc and a daemon for precise computation of system usage
+#execution is sequential at this point, and the sample parameter delays data for the
+#next time series
 work_dir = os.path.join(os.path.sep, os.path.dirname(os.path.realpath(__file__)))
 debug_log = os.path.join(os.path.sep, work_dir, 'debug.log')
-
 logging.basicConfig(filename=debug_log, format='%(asctime)s %(levelname)s:%(message)s',
                     filemode='a', level=logging.DEBUG)
+
 
 def main():
     daemon_context = daemon.DaemonContext(files_preserve = [logging.FileHandler(debug_log)])
@@ -62,6 +50,17 @@ def main():
         SwapMetrics().create_timeseries()
         NetIoMetrics().create_timeseries()
         DiskIoMetrics().create_timeseries()
-            
+
+        
 if __name__ == '__main__':
+    
+    if len(sys.argv) < 2:
+        sample = 15 #how many sample to currently grab for each graph
+        print 'You can send the number of samples, as a parameter, needed for the graphs, default is 15'
+    else:
+        try:
+            sample = int(sys.argv[1])
+        except ValueError:
+            print "That's not an integer, reverting to default 15 samples"
+            sample = 15
     main()
