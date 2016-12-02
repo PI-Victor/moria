863ba110899b800b045b6c30117f003754ceb49b
Victor Palade <palade.ionut@gmail.com>
2016-12-02 18:32:36 +0100

    rename to src

---
+++ src/classfactory.py
@@ -0,0 +1,209 @@
+import time
+import os
+import psutil
+import pygal
+from pygal.style import NeonStyle
+from mongoengine import connection, connect
+from mongo_odm import CpuLoadDoc, VMemDoc, VSwapDoc, NetIoDoc, DiskIoDoc
+
+
+work_dir = os.path.join(os.path.sep, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+graph_dir = os.path.join(os.path.sep, work_dir, 'graphs')
+
+
+class BaseClass(object):
+    
+    def __init__(self, x, y):
+        self.graph_fill = True
+        self.graph_width = x
+        self.graph_height = y
+        self.legend = True
+
+        
+    def create_timeseries(self, **kwargs):
+        self.time_series = {}
+        for point in range(self.sample):
+            datapoints = []
+            kpi_tuple = self.psfunct(**kwargs)
+            [self.time_series.setdefault(metric,[]) for metric in kpi_tuple._fields]
+            [values.append(getattr(kpi_tuple, metric)) for metric, values in self.time_series.items()]
+            time.sleep(self.interval)
+        self.push_to_mongodb()
+        self.create_graph(self.time_series)
+
+        
+    def push_to_mongodb(self):
+        # leave this simple for now, try to access the db for the object, otherwise, return
+        try:
+            connect('kpidb')
+            print("Connection accepted, pushing to mongo database: 'kpidb' ")
+            print(self.message)
+        except connection.ConnectionError as e:
+            print('Connection to the db refused, running "on-the-fly mode", no history available')
+            return 
+
+        
+#        commit_fields = ''
+#        for dbfield, mapped in self.document._meta_map.items():
+#            for datapoint in time_series:
+#                if datapoint == dbfield:
+#                    print time_series[datapoint]
+#                    commit_fields =  "{}, {} = {}".format(commit_fields, mapped, time_series[datapoint])
+        
+#        self.document.save(pushdocument)
+
+
+    def create_graph(self, kpi_list):
+        labels = []
+        for label in kpi_list:
+            labels.append(label)
+            chart = pygal.Line(fill=True, width=self.graph_width, height=self.graph_height, title=self.graph_tag, 
+                               legend_at_bottom=self.legend)
+            chart.x_labels = map(str, range(0, self.sample))
+            for line, series in kpi_list.items():
+                chart.add(line,series)
+            chart.render_to_file(os.path.join(os.path.sep, graph_dir, self.template))
+    
+
+class CpuMetrics(BaseClass):
+    
+    def __init__(self, template='cpu_load.svg', graph_tag='Cpu Load Total', sample=5, interval=1):
+        self.message = 'Cpu Processing started'
+        self.template = template
+        self.graph_tag = graph_tag
+        self.psfunct = psutil.cpu_times_percent
+        self.sample = sample
+        self.interval = interval
+        self.percpu = False
+        self.document = CpuLoadDoc
+        super(CpuMetrics, self).__init__()
+
+        
+    def create_timeseries(self):
+        super(CpuMetrics, self).create_timeseries(interval=self.interval, percpu=self.percpu)
+
+        
+    def push_to_mongodb(self):
+        super(CpuMetrics, self).push_to_mongodb()
+        #this triggers my OCD, but i can't find a better solution for inserting values in the db
+        #at this point, will leave it as it is, until i figure it out. boooo!
+        cpuloaddoc = self.document(
+            cpu_softirq = self.time_series['softirq'],
+            cpu_iowait = self.time_series['iowait'],
+            cpu_sys = self.time_series['system'],
+            cpu_guest = self.time_series['guest'],
+            cpu_idle = self.time_series['idle'],
+            cpu_user = self.time_series['user'],
+            cpu_guestnice = self.time_series['guest_nice'],
+            cpu_irq = self.time_series['irq'],
+            cpu_steal = self.time_series['steal'],
+            cpu_nice = self.time_series['nice'],
+        )
+        cpuloaddoc.save()
+    
+class VmMetrics(BaseClass):
+    
+    def __init__(self, template='vmem.svg', graph_tag='Virtual Memory', sample=5, interval=5):
+        self.message = 'Fetching Virtual Memory Metrics'
+        self.template = template
+        self.graph_tag = graph_tag
+        self.sample = sample
+        self.interval = interval
+        self.psfunct = psutil.virtual_memory
+        self.document = VMemDoc
+        super(VmMetrics, self).__init__()
+
+        
+    def push_to_mongodb(self):
+        super(VmMetrics, self).push_to_mongodb()
+        vmdoc = self.document(
+            vm_total = self.time_series['total'],
+            vm_available = self.time_series['available'],
+            vm_percent = self.time_series['percent'],
+            vm_used = self.time_series['used'],
+            vm_free = self.time_series['free'],
+            vm_active = self.time_series['active'],
+            vm_inactive = self.time_series['inactive'],
+            vm_buffers = self.time_series['buffers'],
+            vm_cached = self.time_series['cached'],
+        )
+        vmdoc.save()
+
+        
+class SwapMetrics(BaseClass):
+    
+    def __init__(self, template='swap_mem.svg', graph_tag='Swap memory', sample=5, interval=5):
+        self.message = 'Fetching Swap Memory Metrics'
+        self.template = template
+        self.graph_tag = graph_tag
+        self.sample = sample
+        self.interval = interval
+        self.psfunct = psutil.swap_memory
+        self.document = VSwapDoc
+        super(SwapMetrics, self).__init__()
+
+        
+    def push_to_mongodb(self):
+        super(SwapMetrics, self).push_to_mongodb()
+        vswapdoc = self.document(
+            vs_used = self.time_series['used'],
+            vs_total = self.time_series['total'],
+            vs_free = self.time_series['free'],
+            vs_percent = self.time_series['percent'],
+            vs_sin = self.time_series['sin'],
+            vs_sout = self.time_series['sout'],
+        )
+        vswapdoc.save()
+
+
+class NetIoMetrics(BaseClass):
+    
+    def __init__(self, template='netio.svg', graph_tag='Network IO', sample=5, interval=5):
+        self.message = 'Fetching NIC Usage Metrics'
+        self.template = template
+        self.graph_tag = graph_tag
+        self.sample = sample
+        self.interval = interval
+        self.psfunct = psutil.net_io_counters
+        self.document = NetIoDoc
+        super(NetIoMetrics, self).__init__()
+
+        
+    def push_to_mongodb(self):
+        super(NetIoMetrics, self).push_to_mongodb()
+        netiodoc = self.document(
+            net_packsent = self.time_series['packets_sent'],
+            net_bytrecv = self.time_series['bytes_recv'],
+            net_packrecv = self.time_series['packets_recv'],
+            net_dropin = self.time_series['dropin'],
+            net_dropout = self.time_series['dropout'],
+            net_bytsent = self.time_series['bytes_sent'],
+            net_errout = self.time_series['errout'],
+            net_errin = self.time_series['errin'],
+        )
+        netiodoc.save()
+
+class DiskIoMetrics(BaseClass):
+    
+    def __init__(self, template='diskio.svg', graph_tag='Diso IO Metrics', sample=5, interval=5):
+        self.message = 'Fetching Disk IO'
+        self.template = template
+        self.graph_tag = graph_tag
+        self.sample = sample
+        self.interval = interval
+        self.psfunct = psutil.disk_io_counters
+        self.document = DiskIoDoc
+        super(DiskIoMetrics, self).__init__()
+
+        
+    def push_to_mongodb(self):
+        super(DiskIoMetrics, self).push_to_mongodb()
+        diskiodoc = self.document(
+            disk_writebyte = self.time_series['write_bytes'],
+            disk_readcount = self.time_series['read_count'],
+            disk_writecount = self.time_series['write_count'],
+            disk_readtime = self.time_series['read_time'],
+            disk_readbyte = self.time_series['read_bytes'],
+            disk_writetime = self.time_series['write_time'],
+        )
+        diskiodoc.save()
--- systemload.py
+++ systemload.py
@@ -1,26 +1,5 @@
-"""Author: https://github.com/PI-Victor
-This project has no purpose but the learning mongodb, mongoengine, pygal
-and the handling of Data series events, metrics and KPIs
-
-The MIT License (MIT)
-Copyright (c)
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the "Software"), 
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-and/or sell copies of the Software, and to permit persons to whom the 
-Software is furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-
-"""Something related to check out:
+Something related to check out:
 http://jsdatav.is/chap01.html
 """
 
@@ -29,31 +8,38 @@ import os
 import sys
 import logging
 import daemon
-from collection.classfactory import CpuMetrics, VmMetrics
-from collection.classfactory import NetIoMetrics, DiskIoMetrics, SwapMetrics
+from src.classfactory import CpuMetrics, VmMetrics
+from src.classfactory import NetIoMetrics, DiskIoMetrics, SwapMetrics
 
-#TODO : Must implement multiproc and a daemon for precise computation of system usage
-#execution is sequential at this point, and the sample parameter delays data for the
-#next time series
-work_dir = os.path.join(os.path.sep, os.path.dirname(os.path.realpath(__file__)))
+# FIXME: move this to a config
+work_dir = os.path.join(
+    os.path.sep,
+    os.path.dirname(os.path.realpath(__file__))
+)
 debug_log = os.path.join(os.path.sep, work_dir, 'debug.log')
-logging.basicConfig(filename=debug_log, format='%(asctime)s %(levelname)s:%(message)s',
-                    filemode='a', level=logging.DEBUG)
+logging.basicConfig(
+    filename=debug_log,
+    format='%(asctime)s %(levelname)s:%(message)s',
+    filemode='a', level=logging.DEBUG
+)
 
+
+def load_functions():
+    CpuMetrics().create_timeseries()
+    VmMetrics().create_timeseries()
+    SwapMetrics().create_timeseries()
+    NetIoMetrics().create_timeseries()
+    DiskIoMetrics().create_timeseries()
 
 def main():
-    daemon_context = daemon.DaemonContext(files_preserve = [logging.FileHandler(debug_log)])
-    #with daemon_context:
-    while True:
-        CpuMetrics().create_timeseries()
-        VmMetrics().create_timeseries()
-        SwapMetrics().create_timeseries()
-        NetIoMetrics().create_timeseries()
-        DiskIoMetrics().create_timeseries()
+    daemon_context = daemon.DaemonContext(
+        files_preserve = [logging.FileHandler(debug_log)]
+    )
+    with daemon_context:
+        load_functions()
 
-        
+
 if __name__ == '__main__':
-    
     if len(sys.argv) < 2:
         sample = 15 #how many sample to currently grab for each graph
         print 'You can send the number of samples, as a parameter, needed for the graphs, default is 15'
